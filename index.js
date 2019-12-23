6e1d32e5cef23ba4988f753116b3d8e374ed68d5
Phiphiste <phiram2306@gmail.com>
2019-12-23 15:10:17 +0100

    verification

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get("/", (req, res) => {
 	res.send({ hello: "world" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, function() {
-	console.log('App listening on port ${PORT}');
+	console.log('App listening on port 8000');
 });
 
